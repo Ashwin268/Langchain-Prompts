52b3dbe21071b5c93fbf7d28e5d9d38edb95f121
Ashwin M Nayak <ashwinnayak268@gmail.com>
2025-09-05 06:50:36 +0530

    Add Streamlit app - Google Gemini API
    - Integrated Google Gemini (gemini-1.5-flash) with LangChain
    - Implemented prompt template using JSON (template_generator.json) for flexibility
    - Built a simple chain to generate summaries and display results in app

---
+++ prompt_ui3.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from dotenv import load_dotenv
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate, load_prompt
+
+
+load_dotenv()
+# api_key = os.getenv("GOOGLE_API_KEY")
+model = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    temperature=0.3,
+)
+
+st.title("Summarize research paper using Google Gemini Model")
+
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+template = load_prompt('template_generator.json') 
+
+btn  = st.button("Summarize")
+if btn:
+    chain = template | model
+    response = chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
+    
+    st.write("### Gemini says:")
+    st.write(response.content)
